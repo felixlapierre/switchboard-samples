57fe126574c12862a2c97d06ee5c1aeb2d55b44d
Maxim Pobudzey <maxim.pobudzey@net4user.com>
2020-10-08 13:32:15 -0400

    Added calls to endpoints for registering the receiver and sender

--- receiver.py
+++ receiver.py
@@ -1,12 +1,28 @@
 import click
 import subprocess
+import requests
 
 @click.command()
 @click.option('--ip', required=True)
 def receive(ip):
-    subprocess.call(['ffplay', '-autoexit', f'udp://{ip}:23000'])
-    # The above simply runs the following command
+    # Register the receiver with the service
+    serial_number = 'z7VBn9aK'
+    response = requests.get(f'http://localhost:8080/device/{serial_number}')
+    if response.status_code == 200:
+        click.echo(f'Device with serial number {serial_number} is already registered.')
+    else:
+        payload = {'serialNumber': serial_number, 'displayName': 'sample_receiver', 'status': 'Running'}
+        url = 'http://localhost:8080/device'
+        r = requests.post(url, json = payload)
+        payload2 = {'serialNumber': serial_number}
+        url2 = 'http://localhost:8080/decoder'
+        r2 = requests.post(url2, json = payload2)
+        if r2.status_code == 201:
+            click.echo(f'Decoder with serial number {serial_number} has been successfully registered.')
+    
+    # The following simply runs the following command
     # ffplay -autoexit udp://<ip>:23000
+    subprocess.call(['ffplay', '-autoexit', f'udp://{ip}:23000'])
 
 if __name__ == '__main__':
     receive()
--- sender.py
+++ sender.py
@@ -1,15 +1,31 @@
 import ffmpeg
 import click
+import requests
 
 @click.command()
 @click.option('--file', required=True)
 @click.option('--ip', required=True)
 def send(file, ip):
+    # Register the sender with the service
+    serial_number = 'd7TxFn7o'
+    response = requests.get(f'http://localhost:8080/device/{serial_number}')
+    if response.status_code == 200:
+        click.echo(f'Device with serial number {serial_number} is already registered.')
+    else:
+        payload = {'serialNumber': serial_number, 'displayName': 'sample_sender', 'status': 'Running'}
+        url = 'http://localhost:8080/device'
+        r = requests.post(url, json = payload)
+        payload2 = {'serialNumber': serial_number}
+        url2 = 'http://localhost:8080/encoder'
+        r2 = requests.post(url2, json = payload2)
+        if r2.status_code == 201:
+            click.echo(f'Encoder with serial number {serial_number} has been successfully registered.')
+    
+    # The following simply runs the following command
+    # ffmpeg -i <input> -f mpegts udp://<ip>:23000
     stream = ffmpeg.input(file)
     stream = ffmpeg.output(stream, f'udp://{ip}:23000', format = 'mpegts')
     ffmpeg.run(stream)
-    # The above simply runs the following command
-    # ffmpeg -i <input> -f mpegts udp://<ip>:23000
 
 if __name__ == '__main__':
     send()
